db929da1c49fb367895a8226e06d7b1f830e4410
Matthias Schenk <Matthias.Schenk@cryptshare.com>
2019-03-25 13:27:07 +0100

    Added Spring Security (part 2), Added basic error handling

--- src/main/java/com/poisonedyouth/SpringBootSample/configuration/WebSecurityConfig.java
+++ src/main/java/com/poisonedyouth/SpringBootSample/configuration/WebSecurityConfig.java
@@ -1,25 +1,30 @@
 package com.poisonedyouth.SpringBootSample.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	// Create Users
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/", "/*todo*/**").access("hasRole('User')").and().formLogin();
+		http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/", "/*todo*/**").access("hasRole('USER')").and().formLogin();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("matthias").password("passw0rd").roles("USER", "ADMIN");
-		super.configure(auth);
+		auth.inMemoryAuthentication().withUser("matthias").password(getPasswordEncoder().encode("passw0rd")).roles("USER", "ADMIN");
+		auth.inMemoryAuthentication().withUser("in28minutes").password(getPasswordEncoder().encode("dummy")).roles("USER", "ADMIN");
 	}
 
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
